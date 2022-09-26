47eaa540d92e2dfccf271c3d628e0c6b17a52294
Candace Eckels <candace.eckels@gmail.com>
2022-09-26 09:27:44 -0600

    Took me 30 minutes to realize that the database was named the same as last inning

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_19_202642) do
+ActiveRecord::Schema.define(version: 2022_09_26_152347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 2022_09_19_202642) do
 
   create_table "hospitals", force: :cascade do |t|
     t.string "name"
+  end
+
+  create_table "patients", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "doctors", "hospitals"
